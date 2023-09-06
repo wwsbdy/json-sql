2d89291465fc4eec0254c26c3b4897d0c6417cfc
zhoujie <wwsbdy@163.com>
2023-09-06 19:51:46 +0800

    创建导出按钮

---
+++ src/main/java/com/zj/demoplugin/table/Table.java
@@ -0,0 +1,91 @@
+package com.zj.demoplugin.table;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.ui.AnActionButton;
+import com.intellij.ui.ToolbarDecorator;
+import com.intellij.ui.table.TableView;
+import com.intellij.util.ui.ListTableModel;
+import com.zj.demoplugin.entity.JsonInfo;
+import com.zj.demoplugin.entity.MyJson;
+import com.zj.demoplugin.form.sql.SqlDialog;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * @author 19242
+ */
+public class Table {
+
+    /**
+     * 插件表格
+     *
+     * @param jsonInfo
+     * @return
+     */
+    public static JPanel create(@NotNull Project project, @NotNull JsonInfo jsonInfo) {
+        // 创建表格模型
+        ListTableModel<MyJson> dataModel = new ListTableModel<>(jsonInfo.getFields());
+        // 创建JTable表格组件
+        TableView<MyJson> table = new TableView<>(dataModel);
+        table.setColumnSelectionAllowed(true);
+        // 固定表头不可移动
+        table.getTableHeader().setReorderingAllowed(false);
+        // 绑定结果
+        dataModel.addRows(jsonInfo.getRows());
+        // 创建装饰器实例
+        ToolbarDecorator decorator = ToolbarDecorator.createDecorator(table, null);
+        // 禁用自带的按钮
+        disableButton(decorator);
+        // 编辑sql按钮
+        AnActionButton editSql = new AnActionButton("SQL查询", AllIcons.Actions.Find) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                SqlDialog formTestDialog = new SqlDialog(project, jsonInfo);
+                formTestDialog.show();
+                // 重新赋值
+                dataModel.setColumnInfos(jsonInfo.getFields());
+                dataModel.setItems(jsonInfo.getRows());
+            }
+        };
+        // 重置按钮
+        AnActionButton reset = new AnActionButton("重置", AllIcons.General.Reset) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                jsonInfo.resetSql();
+                dataModel.setColumnInfos(jsonInfo.getFields());
+                dataModel.setItems(jsonInfo.getRows());
+            }
+        };
+        // 导出按钮
+        AnActionButton export = new AnActionButton("导出", AllIcons.Actions.Commit) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                jsonInfo.resetSql();
+                dataModel.setColumnInfos(jsonInfo.getFields());
+                dataModel.setItems(jsonInfo.getRows());
+            }
+        };
+        decorator.addExtraAction(editSql);
+        decorator.addExtraAction(reset);
+        decorator.addExtraAction(export);
+        return decorator.createPanel();
+    }
+
+    /**
+     * 禁用自带的按钮
+     *
+     * @param decorator
+     */
+    private static void disableButton(ToolbarDecorator decorator) {
+        // 禁用新增删除移动按钮
+        decorator.disableAddAction();
+        decorator.disableDownAction();
+        decorator.disableRemoveAction();
+        decorator.disableUpAction();
+        decorator.disableUpDownActions();
+    }
+
+}
--- src/main/java/com/zj/demoplugin/table/TableRunner.java
+++ src/main/java/com/zj/demoplugin/table/TableRunner.java
@@ -10,16 +10,9 @@ import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.execution.ui.RunContentManager;
 import com.intellij.execution.ui.RunnerLayoutUi;
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
-import com.intellij.ui.AnActionButton;
-import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.content.Content;
-import com.intellij.ui.table.TableView;
-import com.intellij.util.ui.ListTableModel;
 import com.zj.demoplugin.entity.JsonInfo;
-import com.zj.demoplugin.entity.MyJson;
-import com.zj.demoplugin.form.sql.SqlDialog;
 import com.zj.demoplugin.utils.MyExecutorUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -41,7 +34,7 @@ public class TableRunner {
         this.project = project;
     }
 
-    public void run(JsonInfo jsonInfo) {
+    public void run(@NotNull JsonInfo jsonInfo) {
         // 返回定义的 Executor
         Executor executor = MyExecutorUtil.getRunExecutorInstance(TableExecutor.PLUGIN_ID);
         if (executor == null) {
@@ -73,56 +66,12 @@ public class TableRunner {
         }, new DefaultExecutionResult(), layoutUi);
         descriptor.setExecutionId(System.nanoTime());
 
-        JComponent jComponent = getTablePanel(jsonInfo);
+        JComponent jComponent = Table.create(project, jsonInfo);
         final Content content = layoutUi.createContent("contentId", jComponent, "displayName2", AllIcons.Debugger.Console, jComponent);
         content.setCloseable(false);
         layoutUi.addContent(content);
 
         RunContentManager.getInstance(project).showRunContent(executor, descriptor);
     }
-
-    private JPanel getTablePanel(JsonInfo jsonInfo) {
-        // 创建表格模型
-        ListTableModel<MyJson> dataModel = new ListTableModel<>(jsonInfo.getFields());
-        // 创建JTable表格组件
-        TableView<MyJson> table = new TableView<>(dataModel);
-        table.setColumnSelectionAllowed(true);
-        // 固定表头不可移动
-        table.getTableHeader().setReorderingAllowed(false);
-        // 绑定结果
-        dataModel.addRows(jsonInfo.getRows());
-        // 创建装饰器实例
-        ToolbarDecorator decorator = ToolbarDecorator.createDecorator(table, null);
-        // 禁用新增删除移动按钮
-        decorator.disableAddAction();
-        decorator.disableDownAction();
-        decorator.disableRemoveAction();
-        decorator.disableUpAction();
-        decorator.disableUpDownActions();
-        // 编辑sql按钮
-        AnActionButton editSql = new AnActionButton("SQL查询", AllIcons.Actions.Find) {
-            @Override
-            public void actionPerformed(@NotNull AnActionEvent e) {
-                SqlDialog formTestDialog = new SqlDialog(project, jsonInfo);
-                formTestDialog.show();
-                // 重新赋值
-                dataModel.setColumnInfos(jsonInfo.getFields());
-                dataModel.setItems(jsonInfo.getRows());
-            }
-        };
-        decorator.addExtraAction(editSql);
-        // 重置按钮
-        AnActionButton reset = new AnActionButton("重置", AllIcons.General.Reset) {
-            @Override
-            public void actionPerformed(@NotNull AnActionEvent e) {
-                jsonInfo.resetSql();
-                dataModel.setColumnInfos(jsonInfo.getFields());
-                dataModel.setItems(jsonInfo.getRows());
-            }
-        };
-        decorator.addExtraAction(reset);
-        return decorator.createPanel();
-    }
-
 
 }
