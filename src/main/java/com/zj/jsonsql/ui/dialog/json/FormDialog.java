5571f3d1276f2332bff43a49f3272b5f2419ab7f
zhoujie <arthur_zhou@intsig.net>
2023-12-20 15:19:09 +0800

    添加注释

--- src/main/java/com/zj/jsonsql/ui/dialog/json/FormDialog.java
+++ src/main/java/com/zj/jsonsql/ui/dialog/json/FormDialog.java
@@ -1,38 +1,22 @@
 package com.zj.jsonsql.ui.dialog.json;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.Feature;
-import com.google.common.collect.Lists;
 import com.intellij.json.JsonLanguage;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.util.ui.JBUI;
-import com.zj.jsonsql.constant.Constant;
-import com.zj.jsonsql.entity.Field;
-import com.zj.jsonsql.entity.JsonInfo;
-import com.zj.jsonsql.entity.Row;
-import com.zj.jsonsql.enums.JsonEnum;
-import com.zj.jsonsql.enums.NoticeEnum;
 import com.zj.jsonsql.ui.edit.CustomEditorField;
-import com.zj.jsonsql.ui.table.TableRunner;
-import com.zj.jsonsql.utils.JsonUtil;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
+import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.util.List;
-import java.util.*;
+import java.util.Objects;
 
 /**
  * @author arthur_zhou
  */
+@Getter
 public class FormDialog extends DialogWrapper {
 
     private final Project project;
@@ -85,72 +69,7 @@ public class FormDialog extends DialogWrapper {
         submit.setVerticalAlignment(SwingConstants.CENTER);
         south.add(submit);
         //按钮事件绑定
-        submit.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                //获取到name和age
-                String jsonStr = jsonContent.getText();
-                JSONArray jsonArray = null;
-                try {
-                    Object parse = JSON.parse(jsonStr, Feature.OrderedField);
-                    if (parse instanceof JSONObject) {
-                        jsonArray = new JSONArray();
-                        jsonArray.add(parse);
-                    } else if (parse instanceof JSONArray) {
-                        jsonArray = JSONArray.parseArray(jsonStr, Feature.OrderedField);
-                    }
-                } catch (Exception e) {
-                    Messages.showErrorDialog(project, NoticeEnum.JSON_ERROR.getMessage(), NoticeEnum.JSON_ERROR.getWarn());
-                    return;
-                }
-                if (Objects.isNull(jsonArray)) {
-                    Messages.showErrorDialog(project, NoticeEnum.JSON_ERROR.getMessage(), NoticeEnum.JSON_ERROR.getWarn());
-                    return;
-                }
-                if (jsonArray.size() > Constant.ROWS_MAX) {
-                    Messages.showErrorDialog(project, NoticeEnum.ROWS_TOO_MANY.getMessage(), NoticeEnum.ROWS_TOO_MANY.getWarn());
-                    return;
-                }
-                List<Row> rowList = new ArrayList<>();
-                Map<String, List<JsonEnum>> columnMap = new LinkedHashMap<>();
-                String onlyFiled = null;
-                for (Object o : jsonArray) {
-                    Row row;
-                    if (Objects.isNull(o) || !(o instanceof JSONObject)) {
-                        // 不是JSONObject, 新定义一个JSONObject放入
-                        if (Objects.isNull(onlyFiled)) {
-                            onlyFiled = Constant.ONLY_FILED + System.currentTimeMillis() / 1000L;
-                        }
-                        row = new Row(new JSONObject().fluentPut(onlyFiled, o));
-                    } else {
-                        row = new Row((JSONObject) o);
-                    }
-                    for (String key : row.keySet()) {
-                        // 可能会出现不同数据里同一个key，value不一样的情况。如：null和string。这时以不是null的为准，其他的情况以最后一个value类型为准
-                        JsonEnum type = JsonUtil.getType(row.get(key));
-                        if (columnMap.containsKey(key)) {
-                            columnMap.get(key).add(type);
-                        } else {
-                            columnMap.put(key, Lists.newArrayList(type));
-                        }
-                    }
-                    rowList.add(row);
-                }
-                if (MapUtils.isEmpty(columnMap) || CollectionUtils.isEmpty(rowList)) {
-                    Messages.showErrorDialog(project, NoticeEnum.JSON_EMPTY.getMessage(), NoticeEnum.JSON_EMPTY.getWarn());
-                    return;
-                }
-                if (columnMap.size() > Constant.COLUMNS_MAX) {
-                    Messages.showErrorDialog(project, NoticeEnum.COLUMNS_TOO_MANY.getMessage(), NoticeEnum.COLUMNS_TOO_MANY.getWarn());
-                    return;
-                }
-                JsonInfo jsonInfo = new JsonInfo(Field.getOriginalField(columnMap), rowList, jsonStr);
-                // 打开表格
-                new TableRunner(project).run(jsonInfo);
-                // 关闭窗口
-                doCancelAction();
-            }
-        });
+        submit.addActionListener(new ButtonAction(this));
         return south;
     }
 
