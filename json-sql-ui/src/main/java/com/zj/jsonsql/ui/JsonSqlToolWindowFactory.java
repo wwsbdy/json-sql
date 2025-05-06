26da58e7a0ebb60fe4cb8d9b0c0ee94b1ee69453
zhoujie <wwsbdy@163.com>
2025-05-06 22:59:35 +0800

    toolwindow添加按钮调整

--- json-sql-ui/src/main/java/com/zj/jsonsql/ui/JsonSqlToolWindowFactory.java
+++ json-sql-ui/src/main/java/com/zj/jsonsql/ui/JsonSqlToolWindowFactory.java
@@ -1,8 +1,10 @@
 package com.zj.jsonsql.ui;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.wm.ToolWindow;
@@ -17,7 +19,6 @@ import com.zj.jsonsql.ui.table.Table;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.Collections;
 
 /**
  * @author : jie.zhou
@@ -47,14 +48,22 @@ public class JsonSqlToolWindowFactory implements ToolWindowFactory {
 
     private void setupAddTabAction(Project project, ToolWindow toolWindow) {
         // 创建"添加Tab"的动作
-        AnAction addTabAction = new AnActionButtonImpl(PluginBundle.get("tool-window.add-tab"), "", AllIcons.General.Add) {
+        AnAction addTabAction = new ToggleAction(PluginBundle.get("tool-window.add-tab"), "", AllIcons.General.Add) {
             @Override
-            public void actionPerformed(@NotNull AnActionEvent e) {
+            public @NotNull ActionUpdateThread getActionUpdateThread() {
+                return ActionUpdateThread.EDT;
+            }
+
+            @Override
+            public boolean isSelected(@NotNull AnActionEvent anActionEvent) {
+                return false;
+            }
+
+            @Override
+            public void setSelected(@NotNull AnActionEvent anActionEvent, boolean b) {
                 addContent(project, toolWindow);
             }
         };
-        // 将动作添加到ToolWindow的标题栏
-        toolWindow.setTitleActions(Collections.singletonList(addTabAction));
         if (toolWindow instanceof ToolWindowEx) {
             ((ToolWindowEx) toolWindow).setTabActions(addTabAction);
         }
