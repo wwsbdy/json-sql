458f5e4ec1db37007db7252e4c4abb194abb51af
jie.zhou <jie.zhou@alwaysmkt.com>
2025-05-13 18:33:20 +0800

    兼容222版本

--- json-sql-ui/src/main/java/com/zj/jsonsql/ui/table/Table.java
+++ json-sql-ui/src/main/java/com/zj/jsonsql/ui/table/Table.java
@@ -1,7 +1,6 @@
 package com.zj.jsonsql.ui.table;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -178,7 +177,7 @@ public class Table {
                 refresh(dataModel, ideaJsonInfo, table);
             }
         };
-        decorator.addExtraActions((AnAction) modifyJson, editSql, reset, export, importExcel);
+        decorator.addExtraActions(modifyJson, editSql, reset, export, importExcel);
         List<AnActionButtonImpl> anActionButtonList = new ArrayList<>();
         anActionButtonList.add(modifyJson);
         anActionButtonList.add(editSql);
@@ -212,7 +211,7 @@ public class Table {
                     toolWindow.getContentManager().setSelectedContent(content);
                 }
             };
-            decorator.addExtraAction((AnAction) copy);
+            decorator.addExtraAction(copy);
             anActionButtonList.add(copy);
         }
         JPanel panel = decorator.createPanel();
